cb5303d496905fa2ceeb627b01dea1e3a0a5fb1a
easel7 <blankeasel7@gmail.com>
2021-09-12 00:10:38 +0800

    renew

--- ToyMC/toymc.cc
+++ ToyMC/toymc.cc
@@ -4,40 +4,55 @@
 int toymc()
 {
     double bkg_exp[3] = {10000.0, 100.0, 1.0};
-    double sig_exp[3] = {500.0, 50.0, 5.0};
-    double sig[3] = {0.};
+    double sig_exp_on[3] = {500.0, 50.0, 5.0};
+    double sig_exp_off[3] = {0.1, 0.1, 0.1};
+    double sig_on[3] = {0.};
+    double sig_off[3] = {0.};
     double bkg[3] = {0.};
     double weight[3] = {0.05, 0.5, 5.0};
     
-    double sign = 0.;
-    double modi_sig = 0.;
+    double sign_on;
+    double sign_off;
+    double modi_sign_on;
+    double modi_sign_off;
+    double modi_sig_on = 0.;
+    double modi_sig_off = 0.; 
     double modi_bkg = 0.;
 
     int bin = 0;
-    double change;
-    double modi_sign;
-    
-    
-    TFile *newfile = TFile::Open("toymc.root", "recreate");
+
+    TFile *newfile = TFile::Open("result.root", "recreate");
     TTree *tree = new TTree("t1","tree");
-    tree->Branch("sign_change",&change,"change/D"); 
+    tree->Branch("sign_on",&sign_on,"sign_on/D");
+    tree->Branch("sign_off",&sign_off,"sign_off/D");
+    tree->Branch("modi_sign_on",&modi_sign_on,"modi_sign_on/D");
+    tree->Branch("modi_sign_off",&modi_sign_off,"modi_sign_off/D");
+     
 
     for(int i = 0; i < 10000; i++)
     {
-        change = 0;
-        modi_sign = 0;
-        modi_sig = 0;
+        
+        modi_sig_on = 0;
+        modi_sig_off = 0;
         modi_bkg = 0; 
+        
         for(int j = 0; j < 3; j++)
         {
-            sig[j] = gRandom->Poisson(sig_exp[j]);
-            bkg[j] = gRandom->Poisson(bkg_exp[j]);
-            modi_sig += sig[j] * weight[j];
-            modi_bkg += bkg[j] * weight[j] * weight[j];
+            sig_on[j]     = gRandom->Poisson(sig_exp_on[j]);
+            sig_off[j]    = gRandom->Poisson(sig_exp_off[j]); 
+            bkg[j]        = gRandom->Poisson(bkg_exp[j]);
+            
+            modi_sig_on  += sig_on[j] * weight[j];
+            modi_sig_off += sig_off[j] * weight[j];
+            modi_bkg     += bkg[j] * weight[j] * weight[j];
         }
-        sign = (sig[0]+sig[1]+sig[2])/ sqrt(bkg[0]+bkg[1]+bkg[2]);
-        modi_sign = modi_sig / sqrt(modi_bkg);
-        change = modi_sign - sign;
+        
+        sign_on  = (sig_on[0]+sig_on[1]+sig_on[2])   / sqrt(bkg[0]+bkg[1]+bkg[2]);
+        sign_off = (sig_off[0]+sig_off[1]+sig_off[2])/ sqrt(bkg[0]+bkg[1]+bkg[2]); 
+
+        modi_sign_on  = modi_sig_on  / sqrt(modi_bkg);
+        modi_sign_off = modi_sig_off / sqrt(modi_bkg);
+        
         // cout << change << endl;
         tree->Fill();
     }
